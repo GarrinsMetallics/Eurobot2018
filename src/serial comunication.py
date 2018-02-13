1f796082131251aef16c31380dbf3930aa219981
xavi rubia <33039980+xavirubia@users.noreply.github.com>
2018-02-13 17:59:41 +0100

    Update serial comunication.py

--- src/serial comunication.py
+++ src/serial comunication.py
@@ -15,22 +15,20 @@ def decoder(cad,num):
         
 
 ardu1 = serial.Serial('/dev/ttyACM0',9600)
-ardu2 = serial.Serial('/dev/ttyACM1',9600)
+#ardu2 = serial.Serial('/dev/ttyACM1',9600)
 #ardu3 = serial.Serial('/dev/ttyACM1',9600)
 # PER SAPIGUER ON ESTAN CONECTATS cmd: ls /dev/tty* sortiran el ACM on estan.
 write=float
 save=1
-while 1:
+while 1:#"void loop" 
 
-    readA1=ardu1.readline()
-    #readA2=ardu1.readline()
-    #readA3=ardu1.readline()
-    numardu1=decoder(readA1,save)
-    #numardu2=decoder(readA2,save)
-    #numardu3=decoder(readA3,save)
+    readA1=ardu1.readline()###desbloqueja l'ardu1 per utilitzarlo
+    #readA2=ardu1.readline()###desbloqueja l'ardu2 per utilitzarlo
+    #readA3=ardu1.readline()###desbloqueja l'ardu3 per utilitzarlo
+    numardu1=decoder(readA1,save)###desbloqueja l'ardu1 per utilitzarlo
+    #numardu2=decoder(readA2,save)###desbloqueja l'ardu2 per utilitzarlo
+    #numardu3=decoder(readA3,save)###desbloqueja l'ardu3 per utilitzarlo
 
     
     ######################  OPERACIONS AMB VALORS DELS ARDUS##############
 
-    if numardu1 <500:
-        ardu2.write("10")
