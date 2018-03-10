724825a134865ff86111bcc53ebd73f770d45296
Juampe López <juampe.lopez@gmail.com>
2018-03-10 10:34:44 +0100

    Added setpoint setter.

--- src/GarrinsMsgSend.py
+++ src/GarrinsMsgSend.py
@@ -14,9 +14,22 @@ SET_STEPS = 7
 REQUEST = 10
 RESPONSE = 11
 
-ser0 = serial.Serial('/dev/ttyUSB0', 9600)
-#ser1 = serial.Serial('/dev/ttyUSB1', 9600)
-#ser2 = serial.Serial('/dev/ttyUSB2', 9600)
+try:
+    ser0 = serial.Serial('/dev/ttyUSB0', 115200)
+except:
+    ser0 = None
+
+try:
+    ser1 = serial.Serial('/dev/ttyUSB1', 115200)
+except:
+    ser1 = None
+    
+try:
+    ser2 = serial.Serial('/dev/ttyUSB2', 115200)
+except:
+    ser2 = None
+    
+arduino_motor_drivers = [ser0, ser1, ser2]
 
 def writeSerialData(ser, msg_cmd, msg_type, msg_value):
     # Write some data: 1 byte command + 4 bytes value
@@ -43,18 +56,26 @@ def getFWVersion(ser):
 def getUptime(ser):
     writeSerialData(ser, RUNNING_TIME, REQUEST, 0)
     return readSerialData(ser)
+    
+def setSpeedSetpoint(ser):
+    writeSerialData(ser, SET_SPEED, REQUEST, 200)
+    return readSerialData(ser)
 
 def main():
     while True:
-        ser0ID = getDriverID(ser0)
-        print ('serial0 ID = '+str(ser0ID))
+        for driver in arduino_motor_drivers:
+            if driver is not None:
+                driverID = getDriverID(driver)
+                print ('serial ID = '+str(driverID))
         
-        ser0FW = getFWVersion(ser0)
-        print ('serial0 FW Version = '+str(ser0FW))
+                driverFW = getFWVersion(driver)
+                print ('serial FW Version = '+str(driverFW))
         
-        ser0Uptime = getUptime(ser0)
-        print ('serial0 Uptime = '+str(ser0Uptime)+' ms')
-        
+                driverUptime = getUptime(driver)
+                print ('serial Uptime = '+str(driverUptime)+' ms')
+                
+                driverSetpoint = setSpeedSetpoint(driver)
+                print ('serial setpoint = '+str(driverSetpoint))
         time.sleep(1)
 
 if __name__ == "__main__":
